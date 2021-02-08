03d640e16faeffe27014f1ca7b458d0a793c91c7
Mario <mario.borrego100@gmail.com>
2021-02-08 19:41:30 +0100

    clase 92. Creación del Servicio de Clientes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: true,
+    firestore:
+        {
+            apiKey: 'AIzaSyA5zy79jkCLsq7mfG4LtYQlDTvig63yUhk',
+            authDomain: 'control-clientes-d5633.firebaseapp.com',
+            projectId: 'control-clientes-d5633',
+            storageBucket: 'control-clientes-d5633.appspot.com',
+            messagingSenderId: '907644011120',
+            appId: '1:907644011120:web:f410abdc313649610c2ab5',
+            measurementId: 'G-N1L1SV874G'
+        }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firestore:
+        {
+            apiKey: 'AIzaSyA5zy79jkCLsq7mfG4LtYQlDTvig63yUhk',
+            authDomain: 'control-clientes-d5633.firebaseapp.com',
+            projectId: 'control-clientes-d5633',
+            storageBucket: 'control-clientes-d5633.appspot.com',
+            messagingSenderId: '907644011120',
+            appId: '1:907644011120:web:f410abdc313649610c2ab5',
+            measurementId: 'G-N1L1SV874G'
+        }
 };
 
 /*
